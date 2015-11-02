9b7792510e2b33166a01d2bff7b8ca7439a80919
Atte Huhtakangas <atte.huhtakangas@hotmail.fi>
2015-11-02 19:39:52 +0200

    Cleanup and comments
    - Added comments for views in side-menu
    - Removed unneeded templates
    - Added project docs

--- www/js/app.js
+++ www/js/app.js
@@ -18,6 +18,7 @@ lukkariApp.run(function ($ionicPlatform) {
     });
 });
 
+// menuContent-view is presented on the main view.
 lukkariApp.config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
         .state('app', {
@@ -43,32 +44,6 @@ lukkariApp.config(function ($stateProvider, $urlRouterProvider) {
                 }
             }
         });
-    /*.state('app.browse', {
-        url: '/browse',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/browse.html'
-            }
-        }
-    })
-    .state('app.playlists', {
-        url: '/playlists',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/playlists.html',
-                controller: 'PlaylistsCtrl'
-            }
-        }
-    })
-    .state('app.single', {
-        url: '/playlists/:playlistId',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/playlist.html',
-                controller: 'PlaylistCtrl'
-            }
-        }
-    });*/
     // if none of the above states are matched, use this as the fallback
     $urlRouterProvider.otherwise('/app/today');
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -43,37 +43,4 @@ lukkariControllers.controller('LukkariCtrl', function ($scope, $ionicModal, $tim
     };
 });
 
-lukkariControllers.controller('TodayController', function ($scope) {
-
-});
-
-/*lukkariControllers.controller('PlaylistsCtrl', function ($scope) {
-    $scope.playlists = [
-        {
-            title: 'Reggae',
-            id: 1
-        },
-        {
-            title: 'Chill',
-            id: 2
-        },
-        {
-            title: 'Dubstep',
-            id: 3
-        },
-        {
-            title: 'Indie',
-            id: 4
-        },
-        {
-            title: 'Rap',
-            id: 5
-        },
-        {
-            title: 'Cowbell',
-            id: 6
-        }
-  ];
-});
-
-lukkariControllers.controller('PlaylistCtrl', function ($scope, $stateParams) {});*/
+lukkariControllers.controller('TodayController', function ($scope, $http) {});
